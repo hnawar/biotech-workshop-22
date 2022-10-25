655be538a7ce133be06a622e76c940271a819afc
Hatem Nawar <hnawar@google.com>
2022-10-25 15:24:40 +0000

    Update profiles with gtest

--- scripts/build/bootstrap.sh
+++ scripts/build/bootstrap.sh
@@ -34,6 +34,7 @@ mkdir /usr/local/bin/nextflow
 mv ./nextflow /usr/local/bin/nextflow
 
 echo "export PATH=/usr/local/bin/nextflow:$PATH" >> /etc/profile
+echo "export BUCKET=${BUCKET_URL}"
 
 # Copy Config and Service files from Bucket
 gsutil cp ${BUCKET_URL}/provisioning/nextflow.config /etc/nextflow.config
@@ -59,6 +60,14 @@ cat << EOF >> /etc/motd
 ░╚════╝░╚══════╝░╚════╝░░╚═════╝░╚═════╝░
 
 EOF
+
+#Copy testdata to GCP
+curl https://raw.githubusercontent.com/nf-core/test-datasets/imcyto/inputs/20191121_BRAC3438.6f.mcd |gsutil cp - ${BUCKET_URL}/data/inputs/20191121_BRAC3438.6f.mcd
+curl https://raw.githubusercontent.com/nf-core/test-datasets/imcyto/inputs/metadata.csv |gsutil cp - ${BUCKET_URL}/data/inputs/metadata.csv
+curl https://raw.githubusercontent.com/nf-core/test-datasets/imcyto/plugins/full_stack_preprocessing.cppipe |gsutil cp - ${BUCKET_URL}/data/plugins/full_stack_preprocessing.cppipe
+curl https://raw.githubusercontent.com/nf-core/test-datasets/imcyto/plugins/ilastik_stack_preprocessing.cppipe |gsutil cp - ${BUCKET_URL}/data/plugins/ilastik_stack_preprocessing.cppipe
+curl https://raw.githubusercontent.com/nf-core/test-datasets/imcyto/plugins/segmentation.cppipe |gsutil cp - ${BUCKET_URL}/data/plugins/segmentation.cppipe
+curl https://raw.githubusercontent.com/nf-core/test-datasets/imcyto/plugins/ilastik_training_params.ilp |gsutil cp - ${BUCKET_URL}/data/plugins/ilastik_training_params.ilp
 
 #Run only once
 touch /etc/startup_was_launched
